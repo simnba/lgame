c806b5333e810d4d8bff5492a8906ae86663994e
Simon Barton <barton@ph1.uni-koeln.de>
2024-02-03 01:06:15 +0100

    Using symmetries.

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,20 @@ bit_t transpose(bit_t b, bool t){
 
 struct board
 {
-	bit_t l1, l2, s1, s2;
+	bit_t l1 = 0, l2 = 0, s1 = 0, s2 = 0;
+	void normalize(){
+
+
+		// L-swap symmetry
+		if(l1 > l2) std::swap(l1,l2);
+		if(s1 > s2) std::swap(s1,s2);
+
+		// x-symmetry
+		if(flip(l1,true,false) < l1){
+			l1 = flip(l1,true,false);
+			l2 = flip(l2,true,false);
+		}
+	}
 	void start(){
 		l1 = (bit_t)1<<5 | (bit_t)1<<9 | (bit_t)1<<13 | (bit_t)1<<14;
 		l2 = (bit_t)1<<1 | (bit_t)1<<2 | (bit_t)1<<6 | (bit_t)1<<10;
@@ -75,12 +88,30 @@ struct board
 		}
 		file.close();
 	}
+
+};
+struct boardHasher
+{
+	std::size_t operator()(board const& b) const {
+		std::uint64_t all =   (std::uint64_t)b.l1<<48 | (std::uint64_t)b.l2<<32
+							| (std::uint64_t)b.s1<<16 | (std::uint64_t)b.s2;
+		return std::hash<std::uint64_t>()(all);
+	}
 };
 
+bool operator == (board const& b1, board const& b2){
+	boardHasher h;
+	return h(b1) == h(b2);
+}
+
 #include <vector>
+#include <unordered_set>
+
 struct solver{
 	std::vector<bit_t> lPositions;
 	std::vector<bit_t> sPositions;
+
+
 
 	solver() {
 		// generate all 48 positions for an L
@@ -106,33 +137,78 @@ struct solver{
 		}
 	}
 
-	void check(fs::path const& filename) const {
-		std::vector<bit_t> ps;
-		for(int i = 0; i< 48;++i){
-			for(int j = 0; j< 48;++j){
-				if((lPositions[i] & lPositions[j]) == 0){
-					ps.push_back(lPositions[i]);
-					ps.push_back(lPositions[j]);
+	void generateEquivalents(board const& b, std::vector<board>& out) const {
+		int i = 0;
+		for (int t = 0; t < 2; ++t) {
+			for (int fy = 0; fy < 2; ++fy) {
+				for (int fx = 0; fx < 2; ++fx) {
+					for (int s = 0; s < 2; ++s) {
+						board b2;
+						b2.l1 = transpose(flip(b.l1, fx, fy), t);
+						b2.l2 = transpose(flip(b.l2, fx, fy), t);
+						if(s) std::swap(b2.l1,b2.l2);
+						out[i++] = b2;
+					}
 				}
 			}
 		}
-		toFile(ps,filename);
 	}
+	void generatePositions(fs::path const& filename) const {
+		std::unordered_set<board, boardHasher> visited;
+		std::vector<board> bs, variants(16);
+		boardHasher h;
+		int nValid = 0, nRejected = 0;
+		for(int i = 0; i < 48; ++i){
+			for(int j = 0; j < 48; ++j){
+				if((lPositions[i] & lPositions[j]) == 0){
+					nValid++;
+					board b;
+					b.l1 = lPositions[i];
+					b.l2 = lPositions[j];
 
-	static void toFile(std::vector<bit_t> const& ps, fs::path const& filename) {
-		std::ofstream file(filename);
-		for (bit_t const& s: ps) {
-			for (int i = 0; i < 16; ++i) {
-				if (s & ((bit_t) 1 << i)) {
-					file << (i % 4) << " " << (i / 4) << " ";
+					generateEquivalents(b, variants);
+
+
+					board bn = *std::min_element(variants.begin(),variants.end(),[&h](board const& l, board const& r){return h(l) < h(r);});
+
+					if(!visited.contains(bn)) {
+						bs.push_back(b);
+						visited.insert(bn);
+					}
+					else
+						nRejected++;
+
+
+//					if(b == bn) {
+//						bs.push_back(b);
+//					}
+//					else
+//						nRejected++;
 				}
 			}
-			file << "\n";
+		}
+		std::cout << "Accepted " << (nValid-nRejected) << "/" << nValid << "\n";
+		toFile(bs,filename);
+	}
+
+	static void toFile(std::vector<board> const& bs, fs::path const& filename) {
+		std::ofstream file(filename);
+		for (board const& b: bs) {
+			for (bit_t const& s: {b.l1,b.l2,b.s1,b.s2}) {
+				for (int i = 0; i < 16; ++i) {
+					if (s & ((bit_t) 1 << i)) {
+						file << (i % 4) << " " << (i / 4) << " ";
+					}
+				}
+				file << "\n";
+			}
 		}
 		file.close();
 	}
 
 };
+
+#include <chrono>
 
 int main() {
 	board b;
@@ -141,8 +217,11 @@ int main() {
 	b.toFile("board.txt");
 
 	solver s;
-	s.check("solver.txt");
 
-	std::cout << "Done!" << std::endl;
+	auto t0 = std::chrono::high_resolution_clock::now();
+	s.generatePositions("solver.txt");
+	auto t1 = std::chrono::high_resolution_clock::now();
+
+	std::cout << "Took " << std::chrono::duration_cast<std::chrono::microseconds>(t1-t0).count() << std::endl;
 	return 0;
 }
